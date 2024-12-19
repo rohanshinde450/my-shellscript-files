7ec844d7857200a488a3829ead47899a88063c5d
rohanshinde450 <rpshinde450@gmail.com>
2024-12-19 16:11:06 +0530

    Create my3.sh

---
+++ my3.sh
@@ -0,0 +1,15 @@
+echo "enter your name:"
+read name
+mkdir $name
+echo "$name directory created"
+cd $name
+echo "nevigated to $name directory"
+touch $name.txt
+echo "$name.txt file is created"
+ls
+echo "see file"
+pwd
+echo "see exact location of file"
+echo "jevan zal ka...?.......hh hh hh...jevan zal ?" > $name.txt
+echo "hoo zal re baba"
+cat $name.txt
